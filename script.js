48ca05bd0854fa9219c3d9d6f4bce7d44aabc14b
Toirov2 <asad010126@gmail.com>
2025-06-27 18:14:20 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+     .chatbot-header button{
+            color: white;
+            width: 30px; 
+            border: none;
+            border-radius: 3px;
+            font-size: 20px;
+            background: red;
+        }
